0d86fba5ce41aa583d9b3b4c1f02e0f1eb0ef701
Nguyễn Thanh Xuân <thanhxuanhd007@gmail.com>
2020-11-20 02:35:48 +0700

    Add Tailwind css add dashboard info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InfoCard from './components/info-card';
+import Sidebar from './components/side-bar';
+import TaskList from './components/tasks-list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex m-auto w-screen">
+      <Sidebar />
+      <div className="w-full h-screen bg-gray-200 text-gray-800">
+        <InfoCard />
+        <TaskList />
+      </div>
     </div>
   );
 }
